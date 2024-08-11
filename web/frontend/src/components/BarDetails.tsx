a864df0a5ebc0b7200274e92407dd66d2abf3ddd
John Toner <john.toner@example.com>
2024-08-11 08:35:52 +0100

    refactoring bar details to use mui components

--- web/frontend/src/components/BarDetails.tsx
+++ web/frontend/src/components/BarDetails.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  List,
+  ListItemText,
+  Avatar,
+  IconButton,
+  Box,
+} from "@mui/material";
 import Pagination from "@mui/material/Pagination";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { MarkerType } from "../PintsContext";
 
 interface BarDetailsProps {
@@ -9,7 +19,7 @@ interface BarDetailsProps {
 
 const BarDetails: React.FC<BarDetailsProps> = ({ selectedMarker }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 6;
+  const itemsPerPage = 10;
 
   const totalPages = Math.ceil(selectedMarker.pintPrices.length / itemsPerPage);
 
@@ -28,30 +38,36 @@ const BarDetails: React.FC<BarDetailsProps> = ({ selectedMarker }) => {
 
   return (
     <Card
+      variant="outlined"
       className="mt-2"
-      style={{ backgroundColor: "#E3F2FD", color: "#0D47A1" }}
+      style={{ marginBottom: "16px", maxHeight: "543px", overflow: "auto" }}
     >
-      <Card.Body>
-        <Card.Title style={{ fontFamily: "serif" }}>
-          {selectedMarker.name}
-        </Card.Title>
-        <Card.Text style={{ fontFamily: "serif" }}>
-          {selectedMarker.description}
-        </Card.Text>
-        <ListGroup variant="flush">
+      <CardHeader
+        avatar={
+          <Avatar aria-label="bar-avatar">
+            {selectedMarker.name.charAt(0)}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={selectedMarker.name}
+        subheader={selectedMarker.description}
+      />
+      <CardContent>
+        <List>
           {currentItems.map((pintPrice, index) => (
-            <ListGroup.Item
-              key={index}
-              style={{ backgroundColor: "#E3F2FD", color: "#0D47A1" }}
-            >
+            <ListItemText key={index}>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <span>{pintPrice.name}</span>
                 <span>£{pintPrice.price.toFixed(2)}</span>
               </div>
-            </ListGroup.Item>
+            </ListItemText>
           ))}
-        </ListGroup>
-        <div
+        </List>
+        <Box
           style={{
             display: "flex",
             justifyContent: "center",
@@ -64,8 +80,8 @@ const BarDetails: React.FC<BarDetailsProps> = ({ selectedMarker }) => {
             onChange={handlePageChange}
             color="primary"
           />
-        </div>
-      </Card.Body>
+        </Box>
+      </CardContent>
     </Card>
   );
 };
